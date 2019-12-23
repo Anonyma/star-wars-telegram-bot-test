3a2a9153039727b658a316f646d6b1b8b533cb27
Zoe <zxenonwolf@gmail.com>
2019-12-23 06:37:52 +0100

    Now the bot can look up any SW character and returns more data

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const token = process.env.TELEGRAM_TOKEN
 const bot = new TelegramBot(token, { polling: true })
 
 
-bot.on(/\/start/, msg => {
-	const chatId = msg.chat.id
-
+bot.on(/\/start/, ({ chat: { id: chatId }, msg }) => {
 	bot.sendMessage(
 		chatId,
 		`Hmm... ${JSON.stringify(msg.from.username)}, I like your name :)`,
@@ -17,68 +15,103 @@ bot.on(/\/start/, msg => {
 		chatId,
 		`Send me a pic, sticker, edit a message or use the following commands: /sendpic, /starwars/`,
 	)
+	bot.sendMessage(
+		chatId,
+		`You may also get info about any Star Wars character by typing sw:nameOfTheCharacter`,
+	)
 })
 
-bot.on('edited_message', msg => {
-	const chatId = msg.chat.id
-
+bot.on('edited_message', ({ chat: { id: chatId } }) => {
 	bot.sendMessage(chatId, 'I read that :O')
 })
 
-bot.on('photo', msg => {
-	const chatId = msg.chat.id
+bot.on('photo', ({ chat: { id: chatId } }) => {
 	bot.sendMessage(
 		chatId,
 		`Why are you sending me a pic? Don't you know I have no eyes?!`,
 	)
 })
-bot.on('sticker', msg => {
-	const chatId = msg.chat.id
+bot.on('sticker', ({ chat: { id: chatId } }) => {
 	bot.sendMessage(
 		chatId,
 		`Why are you sending me a sticker? Don't you know I have no eyes?!`,
 	)
 })
 
-bot.onText(/\/sendpic/, msg => {
+bot.onText(/\/sendpic/, ({ chat: { id: chatId } }) => {
 	bot.sendPhoto(
-		msg.chat.id,
+		chatId,
 		'https://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-29-57b30ad229af3__605.jpg',
 		{ caption: 'Hope this fluffy cutie brightens up your day~ ' },
 	)
 })
-bot.onText(/\/starwars/, async msg => {
-	bot.sendMessage(msg.chat.id, 'What SW character do you want to know more about?', {
+bot.onText(/\/starwars/, async ({ chat: { id: chatId } }) => {
+	bot.sendMessage(chatId, 'Which SW character would you like to know more about? (Look up any character by typing sw:nameOfTheCharacter)', {
 		reply_markup: {
-			keyboard: [['Luke', 'C3PO']],
+			keyboard: [['sw:Luke', 'sw:C-3PO'], ['sw:Leia', 'sw:R2-D2']],
 		},
+		parse_mode: 'HTML'
 	})
 })
 
-bot.on('message', async msg => {
+bot.on('message', async ({ chat: { id: chatId }, text: msgText }) => {
 	let res
+	let mainSpecies
+	let planet
 	if (
-		msg.text
+		msgText
 			.toString()
-			.includes('Luke')
+			.includes('sw:')
 	) {
-		res = await axios
-			.get('https://swapi.co/api/people/1/')
-			.catch(e => console.log(e))
-	}
-	else if (
-		msg.text
-			.toString()
-			.includes('C3PO')
-	) {
-		res = await axios
-			.get('https://swapi.co/api/people/2/')
-			.catch(e => console.log(e))
-	}
+		const cleanString = msgText.toString().replace('sw:', '')
+		const someCharData = `Gimme a sec! i'm going to look up ${cleanString} in my Encyclopedia 📚📖🧐`
+		bot.sendMessage(chatId, someCharData)
 
-	let { height, name, birth_year } = res.data
-	const frasePJ = `${name} is ${height}cm tall and was born in ${birth_year}`
-	bot.sendMessage(msg.chat.id, frasePJ)
+		try {
+
+			res = await axios
+				.get(`https://swapi.co/api/people/?search=${cleanString}`)
+				.catch(e => console.log(e))
+
+
+			if (res['data']['results'].length) {
+				const { height, name, birth_year, species, homeworld } = res['data']['results'][0]
+
+				// TODO: list all the species the character belongs to :)
+				mainSpecies = await axios
+					.get(species[0]).then(res => res.data)
+					.catch(e => console.log(e))
+
+				planet = await axios
+					.get(homeworld).then(res => res.data)
+					.catch(e => console.log(e))
+				console.log(planet);
+
+				const someCharData = `${name} (species: ${mainSpecies['name']}) is ${height}cm tall and was born in ${planet['name']} (year ${birth_year})`
+				bot.sendMessage(chatId, someCharData)
+			}
+			else {
+				console.log('data res'), res;
+				bot.sendMessage(
+					chatId,
+					`Got nothing for that.. character? Sorry!`,
+				)
+			}
+		}
+		catch (e) {
+			console.log(e);
+			bot.sendMessage(
+				chatId,
+				`Oops, something went wrong! Sorry!`,
+			)
+		}
+	}
+	else {
+		bot.sendMessage(
+			chatId,
+			`Uuh... If you want to know more about a certain Star Wars character, you should type sw:nameOfTheCharacter`,
+		)
+	}
 })
 
 bot.on('polling_error', err => console.log(err))
